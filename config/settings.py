852e7638ca18f9accf91fb25af4b43b6a32e09ea
Pavel Hrygoryk <pg@anahorets-iMac.local>
2022-08-31 15:36:26 +0300

    Add Feedlinks entitiy

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'webpack_loader',
     'rest_framework',
     'api.apps.ApiConfig',
+    'import_export',
 ]
 
 MIDDLEWARE = [
--- project/admin.py
+++ project/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
-from .models import Project, Workspace, Company
+from .models import Project, Workspace, Company, Feedlinks
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
 admin.site.register(Project)
 admin.site.register(Workspace)
 admin.site.register(Company)
+
+
+class FeedlinksResource(resources.ModelResource):
+  class Meta:
+    model = Feedlinks
+
+class FeedlinksAdmin(ImportExportModelAdmin):
+   resource_class = FeedlinksResource
+
+admin.site.register(Feedlinks, FeedlinksAdmin )
