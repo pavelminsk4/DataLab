048753a53b9665d1deb8fdce8021ad306cc09601
Pavel Hrygoryk <pg@anahorets-iMac.local>
2022-09-09 17:15:35 +0300

    Add new endpoint

--- api/serializers.py
+++ api/serializers.py
@@ -18,8 +18,17 @@ class WorkspaceSerializer(serializers.ModelSerializer):
   class Meta:
     model = Workspace
     fields = ['id', 'title', 'description', 'members', 'company', 'projects']
+    #fields = '__all__'
 
 class WorkspaceCreateSerializer(serializers.ModelSerializer):
+  projects = ProjectSerializer(many=True)
   class Meta:
     model = Workspace
-    fields = ['title', 'description', 'members']
+    fields = '__all__'
+
+  def create(self, validated_data):
+    workspace = Workspace.objects.create(title=validated_data['title'], description=validated_data['description'])
+    project = Project.objects.create(**validated_data['projects'][0])
+    workspace.members.set(validated_data["members"])
+    workspace.projects.add(project)
+    return workspace
