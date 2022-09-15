b1a086e871b6632a3e989cfe3f07d601914c4ba0
Pavel Hrygoryk <pg@anahorets-iMac.local>
2022-09-15 13:49:08 +0300

    Add Post Import-Export for admin area

--- project/admin.py
+++ project/admin.py
@@ -6,9 +6,8 @@ from import_export import resources
 admin.site.register(Project)
 admin.site.register(Workspace)
 admin.site.register(Company)
-admin.site.register(Post)
 
-
+# === Feedlinks Import-Export
 class FeedlinksResource(resources.ModelResource):
   class Meta:
     model = Feedlinks
@@ -17,3 +16,13 @@ class FeedlinksAdmin(ImportExportModelAdmin):
    resource_class = FeedlinksResource
 
 admin.site.register(Feedlinks, FeedlinksAdmin )
+
+# ==== Post Import-Export
+class PostResource(resources.ModelResource):
+  class Meta:
+    model = Post
+
+class PostAdmin(ImportExportModelAdmin):
+  resource_class = PostResource
+
+admin.site.register(Post, PostAdmin)
