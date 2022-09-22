a1b5b368788c2008f7b04bfbb7be7c0d4316b9d0
Pavel Hrygoryk <pg@anahorets-iMac.local>
2022-09-22 11:57:01 +0300

    ADD: Add keywords filters

--- api/tests.py
+++ api/tests.py
@@ -6,11 +6,39 @@ from project.models import Post
 import json
 
 class SearchTests(APITestCase):
+  global url
+  url = reverse('search')
+
+  def db_seeder(self):
+    Post.objects.create(entry_title='First post title', entry_summary='First post body')
+    Post.objects.create(entry_title='Second post title', entry_summary='Second post body')
+    Post.objects.create(entry_title='Third post', entry_summary='Third post body')
+    Post.objects.create(entry_title='Fourth post', entry_summary='Fourth post body')
+
   def test_search(self):
-    Post.objects.create(entry_title='Good News', entry_summary='Good')
-    Post.objects.create(entry_title='Bad post', entry_summary='The Best')
-    data = {'keywords': ['Good', 'Post'] }
-    url = reverse('search')
+    self.db_seeder()
+    data = {'keywords':['First', 'Post'], 'exceptions':[], 'additions':[]}
     response = self.client.post(url, data, format='json')
     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    self.assertEqual(json.loads(response.content), [{'entry_media_thumbnail_url': None, 'entry_published': None, 'entry_summary': 'Good', 'entry_title':'Good News', 'feed_language': None, 'sentiment': 'positive'}])  
+    self.assertEqual(json.loads(response.content), [{'entry_media_thumbnail_url': None, 'entry_published': None, 'entry_summary': 'First post body', 'entry_title':'First post title', 'feed_language': None, 'sentiment': 'neutral'}])  
+ 
+  def test_search_with_exclusion_words(self):
+    self.db_seeder()
+    data = {'keywords':['post'], 'exceptions':['First'], 'additions':[]}
+    response = self.client.post(url, data, format='json')
+    self.assertEqual(len(json.loads(response.content)), 3)
+    self.assertEqual(len(Post.objects.all()), 4)
+
+  def test_search_with_additional_words(self):
+    self.db_seeder()
+    data = {'keywords':['post'], 'exceptions':[], 'additions':['Third']}
+    response = self.client.post(url, data, format='json')
+    self.assertEqual(json.loads(response.content),  [{'entry_media_thumbnail_url': None, 'entry_published': None, 'entry_summary': 'Third post body', 'entry_title':'Third post', 'feed_language': None, 'sentiment': 'neutral'}] )
+    self.assertEqual(len(Post.objects.all()), 4)
+
+  def test_serch_with_exclusion_and_additional_words(self):
+     self.db_seeder()
+     data = {'keywords':['post'], 'exceptions':['First'], 'additions':['title']}
+     response = self.client.post(url, data, format='json')
+     self.assertEqual(json.loads(response.content), [{'entry_media_thumbnail_url': None, 'entry_published': None, 'entry_summary': 'Second post body', 'entry_title':'Second post title', 'feed_language': None, 'sentiment': 'neutral'}])
+     self.assertEqual(len(Post.objects.all()), 4)
