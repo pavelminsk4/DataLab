57413db3fbdc032e2b6988af12985833401b7a43
Pavel Hrygoryk <pg@anahorets-iMac.local>
2022-09-27 16:41:49 +0300

    ADD: Add source API

--- api/tests.py
+++ api/tests.py
@@ -2,8 +2,9 @@ from django.test import TestCase
 from rest_framework.test import APITestCase
 from rest_framework import status
 from django.urls import reverse
-from project.models import Post, Speech
+from project.models import Post, Speech, Feedlinks
 from accounts.models import department
+from countries_plus.models import Country
 from django.contrib.auth.models import User
 import json
 
@@ -65,3 +66,33 @@ class CurrentUserTests(APITestCase):
     self.assertEqual(json.loads(response.content)['user_profile']['jobtitle'], 'Boss')
     self.assertEqual(json.loads(response.content)['user_profile']['phone'], '+966-12345678')
 
+class SourcesTests(APITestCase):
+  def test_sources_list(self):
+    Feedlinks.objects.bulk_create([Feedlinks(source1='BBC'), Feedlinks(source1='TNT')])
+    url = reverse('sources_list')
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(json.loads(response.content), [{'source1': 'TNT'}, {'source1': 'BBC'}])
+
+class SpeechesTests(APITestCase):
+  def test_speeches_list(self):
+    user = User.objects.create(username='John')
+    self.client.force_authenticate(user=user)
+    Speech.objects.bulk_create([Speech(language='Italy'), Speech(language='Albanian')])
+    url = reverse('speeches_list')
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(json.loads(response.content), [{'language': 'Albanian'}, {'language': 'Italy'}])
+
+class CountriesTests(APITestCase):
+  def create_country(self):
+    Country.objects.create(iso='AF', iso3='AFG', name='Afghanistan', iso_numeric=4, fips='AF', capital='Kabul', area=1, population=1, continent='AS', tld='.af', currency_code='AFN', currency_name='Afghani', phone=93, languages='fa-AF,ps,uz-AF,tk', geonameid=1149361, neighbours='TM,CN,IR,TJ,PK,UZ')
+
+  def test_countries_list(self):
+    user = User.objects.create(username='Fox')
+    self.client.force_authenticate(user=user)
+    self.create_country()
+    url = reverse('countries_list')
+    response = self.client.get(url)
+    self.assertEqual(response.status_code, status.HTTP_200_OK)
+    self.assertEqual(json.loads(response.content), [{'name': 'Afghanistan'}])
