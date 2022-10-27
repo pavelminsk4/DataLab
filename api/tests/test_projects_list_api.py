5a60c2f5da32e81d64c8d2626e6037d7afa4f229
Pavel Hrygoryk <pg@anahorets-iMac.local>
2022-10-27 15:06:25 +0300

    FIX: Modify API

--- api/tests/test_projects_list_api.py
+++ api/tests/test_projects_list_api.py
@@ -7,7 +7,7 @@ import json
 from django.contrib.auth.models import User
 
 class TestsProjectsAPI(APITestCase):
-  def db_seed(self):
+  def test_projects_list_api(self):
     user = User.objects.create_user(id=1, username='user')
     pr1 = Project.objects.create(title='Project1', keywords=['Keyword'], start_search_date=datetime(2022, 10, 10), end_search_date=datetime(2022, 10, 16), creator=user)
     pr2 = Project.objects.create(title='Project2', keywords=['Apple'], start_search_date=datetime(2022, 10, 10), end_search_date=datetime(2022, 10, 16), creator=user)
@@ -15,56 +15,51 @@ class TestsProjectsAPI(APITestCase):
     pr1.save()
     pr2.created_at = datetime(2022, 10, 17)
     pr2.save()
-
-  def test_projects_list_api(self):
-    self.db_seed()
     url = reverse('projects_list')
     response = self.client.get(url)
     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    res = [
-      {
-        "id": 1,
-        "note": None,
-        "title": "Project1",
-        "keywords": ['Keyword'],
-        "ignore_keywords": None,
-        "additional_keywords": None,
-        "max_items": None,
-        "image": None,
-        "arabic_name": None,
-        "english_name": None,
-        "social": False,
-        "online": False,
-        "premium": False,
-        "source": None,
-        "start_search_date": "2022-10-10T00:00:00Z",
-        "end_search_date": "2022-10-16T00:00:00Z",
-        "creator": 1,
-        "workspace": None,
-        "members": [],
-        "created_at": "2022-10-17T00:00:00Z",
-      },
-      {
-        "id": 2,
-        "note": None,
-        "title": "Project2",
-        "keywords": ['Apple'],
-        "ignore_keywords": None,
-        "additional_keywords": None,
-        "max_items": None,
-        "image": None,
-        "arabic_name": None,
-        "english_name": None,
-        "social": False,
-        "online": False,
-        "premium": False,
-        "source": None,
-        "start_search_date": "2022-10-10T00:00:00Z",
-        "end_search_date": "2022-10-16T00:00:00Z",
-        "creator": 1,
-        "workspace": None,
-        "members": [],
-        "created_at": "2022-10-17T00:00:00Z",
-      }
-    ]
-    self.assertEqual(json.loads(response.content), res)
+    res1 = {
+      "id": pr1.id,
+      "note": None,
+      "title": "Project1",
+      "keywords": ['Keyword'],
+      "ignore_keywords": None,
+      "additional_keywords": None,
+      "max_items": None,
+      "image": None,
+      "arabic_name": None,
+      "english_name": None,
+      "social": False,
+      "online": False,
+      "premium": False,
+      "source": None,
+      "start_search_date": "2022-10-10T00:00:00Z",
+      "end_search_date": "2022-10-16T00:00:00Z",
+      "creator": 1,
+      "workspace": None,
+      "members": [],
+      "created_at": "2022-10-17T00:00:00Z",
+    }
+    res2 = {
+      "id": pr2.id,
+      "note": None,
+      "title": "Project2",
+      "keywords": ['Apple'],
+      "ignore_keywords": None,
+      "additional_keywords": None,
+      "max_items": None,
+      "image": None,
+      "arabic_name": None,
+      "english_name": None,
+      "social": False,
+      "online": False,
+      "premium": False,
+      "source": None,
+      "start_search_date": "2022-10-10T00:00:00Z",
+      "end_search_date": "2022-10-16T00:00:00Z",
+      "creator": 1,
+      "workspace": None,
+      "members": [],
+      "created_at": "2022-10-17T00:00:00Z",
+    }
+    self.assertEqual(json.loads(response.content), [res1, res2])
