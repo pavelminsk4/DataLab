9c97f04c14e9116e31487b147a923a7e4e1b6516
Pavel Hrygoryk <pg@anahorets-iMac.local>
2022-11-10 17:06:05 +0300

    tmp

--- widgets/urls.py
+++ widgets/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'widgets'
 
 urlpatterns = [
-  path('/summary_widget/<int:pk>', views.sum_widget, name='summary_widget'),
-  path('/volume_widget/<int:pk>', views.vol_widget, name='volume_widget'),
+  path('summary_widget/<int:pk>', views.sum_widget, name='summary_widget'),
+  path('volume_widget/<int:pk>', views.vol_widget, name='volume_widget'),
   #path('/sentiment_for_period/<int:pk>', views.sntmnt_for_period, name='sentiment_for_period'),
-  path('/clipping_feed_content_widget/<int:pk>', views.clipping_feed_content_widget, name='clipping_feed_content_widget'),
-  path('/top_10_authors_by_volume/<int:pk>', views.top_10_authors_by_volume, name='top_10_authors_by_volume_widget'),
+  path('clipping_feed_content_widget/<int:pk>', views.clipping_feed_content_widget, name='clipping_feed_content_widget'),
+  path('top_10_authors_by_volume/<int:pk>', views.top_10_authors_by_volume, name='top_10_authors_by_volume_widget'),
 ]
