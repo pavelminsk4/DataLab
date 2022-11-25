b24c8ba125f8fabc3c94f672e6fd4da880f493ce
Pavel Hrygoryk <pg@anahorets-iMac.local>
2022-11-25 17:14:25 +0300

    ADD: Add pivot for clipping widget

--- widgets/common_widget/clipping_widget.py
+++ widgets/common_widget/clipping_widget.py
@@ -4,6 +4,21 @@ from django.shortcuts import get_object_or_404
 
 def clp_widget(pk):
   project = get_object_or_404(Project, pk=pk)
+  author_dim_pivot = project.widgets_list_2.clipping_widget.author_dim_pivot
+  country_dim_pivot = project.widgets_list_2.clipping_widget.contry_dim_pivot
+  language_dim_pivot = project.widgets_list_2.clipping_widget.language_dim_pivot
+  source_dim_pivot = project.widgets_list_2.clipping_widget.source_dim_pivot
+  sentiment_dim_pivot =project.widgets_list_2.clipping_widget.sentiment_dim_pivot
+  if author_dim_pivot!=None:
+   posts = posts.filter(entry_author=author_dim_pivot)
+  if country_dim_pivot!=None:
+   posts = posts.filter(feedlink__country=country_dim_pivot)
+  if language_dim_pivot!=None:
+   posts = posts.filter(feed_language__language=language_dim_pivot)
+  if source_dim_pivot!=None:
+   posts = posts.filter(feedlink__source1=source_dim_pivot)
+  if sentiment_dim_pivot!=None:
+   posts = posts.filter(sentiment=sentiment_dim_pivot)
   posts = posts_agregator(project)
   posts = posts.values(
     'entry_title',
