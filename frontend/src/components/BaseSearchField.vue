1ef19c600b146bd276cf79f4ad3ef43952500215
dpk <dpk@anadeainc.com>
2022-12-07 17:46:22 +0300

    ADD: new base search field for additional filters

--- frontend/src/components/BaseSearchField.vue
+++ frontend/src/components/BaseSearchField.vue
@@ -1,9 +1,8 @@
 <template>
   <div
-    :class="['selector', {open: visible}, `selector-${name}`]"
+    :class="['selector', {open: list.length && visible}, `selector-${name}`]"
     :data-value="value"
     :data-list="list"
-    @click="visible = true"
   >
     <div class="label">
       <input
@@ -12,7 +11,6 @@
         :value="modelValue"
         :class="['input', isSearch && 'input-search']"
         :placeholder="placeholder"
-        @focus="visible = true"
         @input="handleInput"
         type="text"
         class="select-search"
@@ -22,13 +20,8 @@
       </div>
       <div v-else-if="!isSearch">{{ value }}</div>
     </div>
-    <ArrowDownIcon
-      class="arrow"
-      :class="{expanded: visible}"
-      @click="toggle()"
-    />
     <div :class="{hidden: !visible, visible}">
-      <ul v-if="visible" class="select-list">
+      <ul v-if="visible && list.length" class="select-list">
         <li
           v-if="isRejectSelection"
           @click="select('Reject selection')"
@@ -48,10 +41,9 @@
     </div>
   </div>
 </template>
+
 <script>
-import ArrowDownIcon from '@/components/icons/ArrowDownIcon'
 export default {
-  components: {ArrowDownIcon},
   emits: ['update:modelValue', 'select-option'],
   props: {
     list: {
@@ -99,6 +91,7 @@ export default {
       this.search = this.currentValue
       this.value = this.currentValue
     }
+
     document.addEventListener('click', this.close)
   },
   computed: {
@@ -108,18 +101,17 @@ export default {
           return item?.toLowerCase().includes(this.modelValue?.toLowerCase())
         })
       }
+
       if (this.isSearch) {
         return this.list.filter((item) => {
           return item?.toLowerCase().includes(this.modelValue?.toLowerCase())
         })
       }
+
       return this.list
     },
   },
   methods: {
-    toggle() {
-      this.visible = !this.visible
-    },
     handleInput(e) {
       this.visible = true
       this.$emit('update:modelValue', e.target.value, this.name)
@@ -132,6 +124,7 @@ export default {
     },
     close() {
       const elements = document.querySelectorAll(`.selector-${this.name}`)
+
       if (!Array.from(elements).find((el) => el.contains(event.target))) {
         this.visible = false
       }
@@ -147,96 +140,116 @@ export default {
   },
 }
 </script>
+
 <style lang="scss" scoped>
 .selector {
   position: relative;
+
   border: 1px solid #2e2f34;
   box-shadow: 0 4px 10px rgba(16, 16, 16, 0.25);
   border-radius: 10px;
   background: rgba(46, 47, 52, 0.5);
+
   cursor: pointer;
-  .arrow {
-    position: absolute;
-    right: 18px;
-    top: 40%;
-    transform: rotateZ(0deg) translateY(0px);
-    transition-duration: 0.3s;
-    transition-timing-function: cubic-bezier(0.59, 1.39, 0.37, 1.01);
-    color: var(--primary-text-color);
-  }
+
   .expanded {
     transform: rotateZ(180deg) translateY(2px);
   }
+
   .label {
     display: block;
+
     padding: 9px 35px 9px 15px;
+
     color: var(--primary-text-color);
     font-size: 14px;
+
     .placeholder {
       color: var(--secondary-text-color);
     }
   }
 }
+
 .select-list {
   position: absolute;
   z-index: 1;
+
   padding: 0;
   margin: 0;
   width: 100%;
   max-height: 250px;
+
   outline: 1px solid var(--primary-button-color);
   border-top: 1px solid var(--modal-line-color);
   box-shadow: 0 3px 4px rgba(5, 95, 252, 0.49);
   border-radius: 0 0 10px 10px;
   background-color: var(--secondary-bg-color);
+
   font-size: 14px;
+
   list-style-type: none;
   overflow-y: auto;
   overflow-x: hidden;
+
   &::-webkit-scrollbar {
     height: 5px;
     width: 5px;
   }
+
   &::-webkit-scrollbar-track {
     background: var(--secondary-bg-color);
     border: 1px solid var(--input-border-color);
     border-radius: 0 10px 10px 0;
   }
+
   &::-webkit-scrollbar-thumb {
     height: 4px;
+
     background: var(--secondary-text-color);
     border-radius: 10px;
   }
 }
+
 .select-search {
   outline: none;
+
   min-width: 100%;
+
   border: none;
-  background: var(--secondary-bg-color);
+  background: transparent;
+
   color: var(--primary-text-color);
 }
+
 .select-item {
   padding: 9px 9px 9px 19px;
+
   color: var(--primary-text-color);
+
   &:hover {
     background: var(--primary-button-color);
   }
 }
+
 .current {
   background: var(--primary-button-color);
 }
+
 .open {
   border: 1px solid var(--primary-button-color);
   border-bottom: none;
   box-shadow: 0 3px 4px rgba(5, 95, 252, 0.49);
   border-radius: 10px 10px 0 0;
 }
+
 .hidden {
   visibility: hidden;
+
   .select-list {
     height: 0;
   }
 }
+
 .visible {
   visibility: visible;
 }
