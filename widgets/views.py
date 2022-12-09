ef2caf2e8b7ef7811f81f88f0428bd9c9f2badfa
Anzhalika Artsiomenka <angelichka21.02@gmail.com>
2022-12-09 15:52:07 +0300

    ADD: Add api for content_volume_top_10_sourse_widget

--- widgets/views.py
+++ widgets/views.py
@@ -10,6 +10,7 @@ from .common_widget.top_10_authors_by_volume_widget import top_10_auth_by_vol_wi
 from .common_widget.top_10_brands_widget import top_10_brands
 from .common_widget.top_10_countries_widget import top_10_countries
 from .common_widget.top_10_languages_widget import top_10_languages
+from .common_widget.content_volume_top_10_source_widget import content_volume_top_10_source
 from .common_widget.clipping_widget import clp_widget
 # from .common_widget.sentiment_for_period import sentiment_for_period
 
@@ -39,3 +40,6 @@ def top_10_countries_widget(request, pk):
 
 def top_10_languages_widget(request, pk):
   return top_10_languages(pk)
+
+def content_volume_top_10_source_widget(request, pk):
+  return content_volume_top_10_source(pk)  
