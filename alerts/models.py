e4531eda30b4c6ae80f83e6cda63ee3b1cf4484b
Pavel <pavelmins4@gmail.com>
2022-12-13 00:37:25 +0300

    FIX: Fix alert initial posts count

--- alerts/models.py
+++ alerts/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from accounts.models import department
-from project.models import Project
+from project.models import Project, Post
 from django.contrib.auth.models import User
+from functools import reduce
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 class Alert(models.Model):
   title = models.CharField(max_length=50)
@@ -14,3 +19,50 @@ class Alert(models.Model):
 
   def __str__(self):
     return self.title
+
+def keywords_posts(keys, posts):
+  posts = posts.filter(reduce(lambda x,y: x | y, [Q(entry_title__contains=key) for key in keys]))
+  return posts
+
+def exclude_keywords_posts(posts, exceptions):
+  to_be_removed = []
+  for post in posts:
+    for word in exceptions:
+      if word in post.entry_title:
+        to_be_removed.append(post.id)
+        break
+  posts = posts.exclude(id__in=to_be_removed)
+  return posts
+
+def additional_keywords_posts(posts, additions):
+  for word in additions:
+    posts = posts.filter(entry_title__contains=word)
+  return posts
+
+def posts_agregator(project):
+  project = get_object_or_404(Project, pk = project.pk)
+  posts = Post.objects.all()
+  posts = keywords_posts(project.keywords, posts)
+  if project.additional_keywords!=[]:
+    posts = additional_keywords_posts(posts, project.additional_keywords)
+  if project.ignore_keywords!=[]:
+    posts = exclude_keywords_posts(posts, project.ignore_keywords)
+  if project.country_filter!=None:
+    posts = posts.filter(feedlink__country=project.country_filter)
+  if project.language_filter!=None:
+    posts = posts.filter(feed_language__language=project.language_filter)
+  if project.source_filter!=None:
+    posts = posts.filter(feedlink__source1=project.source_filter)
+  if project.author_filter!=None:
+    posts = posts.filter(entry_author=project.author_filter)
+  if project.sentiment_filter!=None:
+    posts = posts.filter(sentiment=project.sentiment_filter) 
+  return posts
+
+
+@receiver(post_save, sender=Alert)
+def define_initial_posts_count(sender, instance, created, **kwargs):
+  if created:
+    initial_posts_count = posts_agregator(instance.project).count()
+    instance.privious_posts_count = initial_posts_count
+    instance.save()
