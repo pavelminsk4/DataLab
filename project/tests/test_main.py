2714bf0b594779e5f1ea4e5d785d1087a12fcf34
Pavel Hrygoryk <pg@anahorets-iMac.local>
2023-03-02 17:07:17 +0300

    FIX: Fix tests

--- project/tests/test_main.py
+++ project/tests/test_main.py
@@ -13,7 +13,7 @@ class LoginTests(StaticLiveServerTestCase):
 
   def test_log_in(self):
     user1 = User.objects.create_user(username='admin', password='admin')
-    self.selenium.get('%s%s' % (self.live_server_url, '/dashboard'))
+    self.selenium.get('%s%s' % (self.live_server_url, '/'))
     assert 'Log in' in self.selenium.page_source
     user_name = self.selenium.find_element('id', 'id_username')
     password = self.selenium.find_element('id', 'id_password')
@@ -21,7 +21,7 @@ class LoginTests(StaticLiveServerTestCase):
     user_name.send_keys('admin')
     password.send_keys('admin')
     button.click()
-    assert 'No workspace created' in self.selenium.page_source
+    assert 'Hello' in self.selenium.page_source
     self.selenium.close()
 
 class WorkspaceTests(StaticLiveServerTestCase):
@@ -58,7 +58,10 @@ class WorkspaceTests(StaticLiveServerTestCase):
     self.selenium.get(self.live_server_url + '')  #selenium will set cookie domain based on current page domain
     self.selenium.add_cookie({'name': 'sessionid', 'value': cookie.value, 'secure': False, 'path': '/'})
     self.selenium.refresh() #need to update page for logged in user
-    self.selenium.get('%s%s' % (self.live_server_url, '/dashboard'))
+    self.selenium.get('%s%s' % (self.live_server_url, '/'))
+    assert 'Hello, admin' in self.selenium.page_source
+    module_card_main = self.selenium.find_element(By. CLASS_NAME, 'module-card')
+    module_card_main.click()
     assert 'workspace1' and 'workspace2' in self.selenium.page_source
     assert not 'workspace3' in self.selenium.page_source
     self.selenium.close()
