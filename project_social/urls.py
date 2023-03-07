58ccb08824764dee0e1b4a4ecf7dac96e302f56f
Pavel Hrygoryk <pg@anahorets-iMac.local>
2023-03-07 10:44:08 +0300

    FIX

--- project_social/urls.py
+++ project_social/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
   path("social_workspaces/create/", views.WorkspaceSocialCreate.as_view(),name="social_workspaces_create"),
   path("social_workspaces/update/<int:pk>/",views.WorkspaceSocialUpdate.as_view(),name="social_workspaces_update"),
   path("social_workspaces/delete/<int:pk>/",views.WorkspaceSocialDelete.as_view(),name="social_workspaces_delete"),
-  path("twitter_post_search", views.twitter_posts_search,name="twitter_posts_search"),
+  path("twitter_post_search/", views.twitter_posts_search,name="twitter_posts_search"),
 ]
 
 router.register('projects', ProjectsSotialViewSet)
