70bea57daa9b202b47482820deb16c6263985db0
Artur Aliakseyeu <aae@krononsoft.com>
2023-03-09 14:36:25 +0300

    Rework of interactive widgets tests

--- widgets/common_widget/content_volume_top_5_authors_widget.py
+++ widgets/common_widget/content_volume_top_5_authors_widget.py
@@ -23,11 +23,11 @@ def agregator_results_content_volume_top_authors(posts, smpl_freq):
             list_dates.append({"date": date, "post_count": results[elem][top_authors[elem]][i]['created_count']})
       else:
         list_dates.append({"date": date, "post_count": 0})
-    if (top_authors[elem] == '') or (top_authors[elem] == None) or ('img' in top_authors[elem]) or (top_authors[elem] == 'None') or (top_authors[elem] == 'null') or not top_authors[elem]:    
-      res.append({'Missing in source': list_dates})    
+    if (top_authors[elem] == '') or (top_authors[elem] == None) or ('img' in top_authors[elem]) or (top_authors[elem] == 'None') or (top_authors[elem] == 'null') or not top_authors[elem]:
+      res.append({'Missing in source': list_dates})
     else:
-      res.append({top_authors[elem]: list_dates}) 
-  return res      
+      res.append({top_authors[elem]: list_dates})
+  return res
 
 def content_volume_top_5_authors(request, pk, widget_pk):
   project = Project.objects.get(id=pk)
@@ -36,5 +36,5 @@ def content_volume_top_5_authors(request, pk, widget_pk):
   posts = post_agregetor_for_each_widget(widget, posts)
   body = json.loads(request.body)
   smpl_freq = body['smpl_freq']
-  res = agregator_results_content_volume_top_authors(posts, smpl_freq) 
+  res = agregator_results_content_volume_top_authors(posts, smpl_freq)
   return JsonResponse(res, safe = False)
--- widgets/tests/test_interactive_data_for_widgets.py
+++ widgets/tests/test_interactive_data_for_widgets.py
@@ -7,18 +7,22 @@ from project.models import Post, Project, Speech, Feedlinks
 from django.contrib.auth.models import User
 
 class InteractiveWidgetsTests(APITestCase):
-  def test_top_10_interactive_widgets(self):
+  def setUp(self):
     user = User.objects.create(username='Arturo')
     flink1 = Feedlinks.objects.create(country = 'England', source1='Time')
     sp1 = Speech.objects.create(language='English')
     sp2 = Speech.objects.create(language='Georgian')
-    post1 = Post.objects.create(feedlink=flink1, entry_title='First post title', feed_language=sp1, entry_published=datetime(2021, 9, 3, 6, 37), entry_author='AFP', sentiment='neutral')
-    post2 = Post.objects.create(feedlink=flink1, entry_title='Second post title', feed_language=sp2, entry_published=datetime(2022, 9, 3, 6, 37), entry_author='AFP', sentiment='neutral')
-    pr = Project.objects.create(title='Project1', keywords=['post'], additional_keywords=[], ignore_keywords=[], start_search_date=datetime(2020, 10, 10),
-                                end_search_date=datetime(2023, 10, 16), creator=user, language_dimensions=['English', 'Georgian'], country_dimensions=['England', 'USA'], 
-                                source_dimensions=['Time', 'BBC'], author_dimensions=['AFP'], sentiment_dimensions = ['negative', 'neutral', 'positive'])
+    Post.objects.create(feedlink=flink1, entry_title='First post title', feed_language=sp1, entry_published=datetime(2021, 9, 3, 6, 37), entry_author='AFP', sentiment='neutral')
+    Post.objects.create(feedlink=flink1, entry_title='Second post title', feed_language=sp2, entry_published=datetime(2022, 9, 3, 6, 37), entry_author='AFP', sentiment='neutral')
+    Project.objects.create(title='Project1', keywords=['post'], additional_keywords=[], ignore_keywords=[], start_search_date=datetime(2020, 10, 10),
+                            end_search_date=datetime(2023, 10, 16), creator=user, language_dimensions=['English', 'Georgian'], country_dimensions=['England', 'USA'], 
+                            source_dimensions=['Time', 'BBC'], author_dimensions=['AFP'], sentiment_dimensions = ['negative', 'neutral', 'positive'])
+
+  def test_top_10_interactive_widgets(self):
+    pr = Project.objects.first()
     widget_pk = pr.widgets_list_2.top_10_languages_widget_id
     url = reverse('widgets:interactive_widgets', kwargs={'project_pk':pr.pk, 'widget_pk':widget_pk})
+    post1 = Post.objects.all().get(entry_title='First post title')
     data = {
       'language': 'English',
       'posts_per_page': 10,
@@ -29,15 +33,9 @@ class InteractiveWidgetsTests(APITestCase):
     self.assertEqual(json.loads(response.content)['posts'][0]['id'], post1.id)
 
   def test_sentiment_top_10_interactive_widgets(self):
-    user = User.objects.create(username='Arturo')
-    flink1 = Feedlinks.objects.create(country = 'England', source1='Time')
-    sp1 = Speech.objects.create(language='English')
-    sp2 = Speech.objects.create(language='Georgian')
-    Post.objects.create(feedlink=flink1, entry_title='First post title', feed_language=sp1, entry_published=datetime(2021, 9, 3, 6, 37), entry_author='AFP', sentiment='neutral')
-    Post.objects.create(feedlink=flink1, entry_title='Second post title', feed_language=sp2, entry_published=datetime(2022, 9, 3, 6, 37), entry_author='AFP', sentiment='neutral')
-    pr = Project.objects.create(title='Project1', keywords=['post'], additional_keywords=[], ignore_keywords=[], start_search_date=datetime(2020, 10, 10),
-                                end_search_date=datetime(2023, 10, 16), creator=user, language_dimensions=['English', 'Georgian'], country_dimensions=['England', 'USA'], 
-                                source_dimensions=['Time', 'BBC'], author_dimensions=['AFP'], sentiment_dimensions = ['negative', 'neutral', 'positive'])
+    flink1 = Feedlinks.objects.first()
+    sp2 = Speech.objects.get(language='Georgian')
+    pr = Project.objects.first()
     post3 = Post.objects.create(feedlink=flink1, entry_title='3 post title', feed_language=sp2, entry_published=datetime(2022, 9, 3, 6, 37), entry_author='AFP', sentiment='negative')
     widget_pk = pr.widgets_list_2.sentiment_top_10_languages_widget_id
     url = reverse('widgets:interactive_widgets', kwargs={'project_pk':pr.pk, 'widget_pk':widget_pk})
