c1f6460ed0d88181bb57c10d0d8e918b9af1fd14
Anzhalika Artsiomenka <angelichka21.02@gmail.com>
2023-04-10 15:40:50 +0300

    ADD: Add account analysis workspaces and projects API

---
+++ account_analysis/views.py
@@ -0,0 +1,31 @@
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
+from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from .models import *
+
+class ProjectsAccountAnalysisViewSet(viewsets.ModelViewSet):
+  queryset = ProjectAccountAnalysis.objects.all()
+  serializer_class = ProjectAccountAnalysisSerializer
+
+class WorkspaceAccountAnalysisList(ListAPIView):
+  serializer_class = WorkspaceAccountAnalysisSerializer
+
+  def get_queryset(self):
+    user = self.request.user
+    if not user.is_anonymous:
+      return WorkspaceAccountAnalysis.objects.filter(members=user)
+
+    return WorkspaceAccountAnalysis.objects.none()
+
+class WorkspaceAccountAnalysisCreate(CreateAPIView):
+  queryset = WorkspaceAccountAnalysis.objects.all()
+  serializer_class = WorkspaceCreateSerializer
+
+class WorkspaceAccountAnalysisUpdate(UpdateAPIView):
+  queryset = WorkspaceAccountAnalysis.objects.all()
+  serializer_class = WorkspaceAccountAnalysisSerializer
+
+class WorkspaceAccountAnalysisDelete(DestroyAPIView):
+  queryset = WorkspaceAccountAnalysis.objects.all()
+  serializer_class = WorkspaceAccountAnalysisSerializer
