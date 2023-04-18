f584a6f7295825c8cb205e1c962a6fd7c0bdf3cd
Artur Aliakseyeu <aae@krononsoft.com>
2023-04-18 15:38:44 +0300

    Improvement of the expert mode

--- project/expert_mode.py
+++ project/expert_mode.py
@@ -7,29 +7,45 @@ class Parser:
   def __init__(self, query):
     self.query = query
 
-  def get_boolean_expression(self):
-    parsed_query = self.query
+  fields = {
+    'source': 'feedlink__source1',
+    'country': 'feedlink__country',
+    'author': 'entry_author',
+    'sentiment': 'sentiment',
+    'language': 'feed_language__language__icontains'
+  }
+
+  def get_expression(self):
     parsed_query = self.query.replace('\'','\"').lower()
-    count = len(list(re.finditer(r"\"(.*?)\"", parsed_query)))
-    for c in range(count):
-      ind = list(re.finditer(r"\"(.*?)\"", parsed_query))[c].start()
-      parsed_query = parsed_query[:ind] + 'x = ' + parsed_query[ind:]
-    return parsed_query
+    special = ['!',':','@',',','#','$','%','^','&','*','(',')','-','+','?','_','=','<','>','/','or','and','not']
+    filter_list = list(map(lambda w: w.group(), re.finditer(r'[a-z]+|\"(.*?)\"|\'(.*?)\'|\S', parsed_query)))
+    res = ''
+    i = 0
+    while i < len(filter_list):
+      if filter_list[i] in special:
+        res += filter_list[i] + ' '
+      elif i+2 < len(filter_list) and filter_list[i] in self.fields.keys() and  filter_list[i+1] == ':':
+        res += self.fields[filter_list[i]] + ' = ' + filter_list[i+2] + ' '
+        i +=2
+      else:
+        res += 'entry_title__icontains = ' + filter_list[i] + ' '
+      i += 1
+    return res
 
   def can_parse(self):
     try:
-      parse(self.get_boolean_expression())
+      parse(self.get_expression())
     except:
       return False
     else:
       return True
 
   def get_filter_query(self):
-    return self.simple_parse(parse(self.get_boolean_expression()))
+    return self.simple_parse(parse(self.get_expression()))
 
   def simple_parse(self, cond):
     if hasattr(cond, 'data'):
-      return Q(entry_title__contains = cond.value)
+      return Q(**{cond.name : cond.value})
     if not hasattr(cond, 'logicop'):
       return cond
     elif(cond.logicop == 'or'):
