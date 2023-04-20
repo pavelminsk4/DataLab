579b8ce28a3d79e504184188027dae73c06fa591
dpk <dpk@anadeainc.com>
2023-04-20 11:59:59 +0300

    ADD: setting section for interactive modal

---
+++ frontend/src/components/icons/PostsDisplayNormalIcon.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg width="20" height="20" viewBox="0 0 20 20" fill="none">
+    <rect x="2" y="3" width="16" height="2" rx="1" fill="currentColor" />
+    <rect x="2" y="7" width="16" height="2" rx="1" fill="currentColor" />
+    <rect x="2" y="11" width="16" height="2" rx="1" fill="currentColor" />
+    <rect x="2" y="15" width="16" height="2" rx="1" fill="currentColor" />
+  </svg>
+</template>
