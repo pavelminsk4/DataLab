98f4811b9be4ce666fd408d35099d20b5c9e5a24
Pavel Hrygoryk <pg@anahorets-iMac.local>
2023-04-21 18:57:25 +0300

    FIX: Fix alert module

--- alerts/services/check_new_posts.py
+++ alerts/services/check_new_posts.py
@@ -1,16 +1,15 @@
 from alerts.services.online_posts_agregator import posts_agregator
 from alerts.services.social_posts_agregator import social_posts_agregator
 
-def check_new_posts(alert):
-  triger_on_every_new_posts = alert.triggered_on_every_n_new_posts
-  previous_posts_count = alert.privious_posts_count
-  if alert.module_type == 'Project':
-    total_posts_count = posts_agregator(alert.module_project_id).count()
-  if alert.module_type == 'ProjectSocial':
-    total_posts_count = social_posts_agregator(alert.module_project_id).count()
+def check_new_posts(item, triggered_on_every_n_new_posts):
+  previous_posts_count = item.previous_posts_count
+  if item.module_type == 'Project':
+    total_posts_count = posts_agregator(item.module_project_id).count()
+  if item.module_type == 'ProjectSocial':
+    total_posts_count = social_posts_agregator(item.module_project_id).count()
   delta = total_posts_count - previous_posts_count
-  if delta >= triger_on_every_new_posts:
-    alert.privious_posts_count = total_posts_count
-    alert.save()
+  if delta >= triggered_on_every_n_new_posts:
+    item.previous_posts_count = total_posts_count
+    item.save()
     return delta
   return False
