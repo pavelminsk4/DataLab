781fcfb8ae30576e0f54f8626e69a891d6c7ade0
Nikita Staselovich <nikitastaselovich@gmail.com>
2023-04-26 13:50:07 +0300

    FIX: fixed empty alert reset

--- frontend/store/mutations.js
+++ frontend/store/mutations.js
@@ -1,5 +1,17 @@
 import {mutator} from '@store/constants'
 
+const initialAlert = {
+  step: 1,
+  title: '',
+  description: '',
+  user: [],
+  department: null,
+  creator: null,
+  alert_condition: '',
+  triggered_on_every_n_new_posts: '',
+  how_many_posts_to_send: '',
+}
+
 export default {
   [mutator.SET_LOADING](state, loading) {
     state.loading = loading
@@ -306,6 +318,8 @@ export default {
   [mutator.SET_NEW_ALERT](state, data) {
     if (data) {
       state.newAlert = {...state.newAlert, ...data}
+    } else {
+      state.newAlert = initialAlert
     }
   },
   [mutator.SET_REPORT_WIDGETS_LIST](state, data) {
--- frontend/store/state.js
+++ frontend/store/state.js
@@ -111,7 +111,12 @@ const initialState = {
     step: 1,
     title: '',
     description: '',
-    projects: [],
+    user: [],
+    department: null,
+    creator: null,
+    alert_condition: '',
+    triggered_on_every_n_new_posts: '',
+    how_many_posts_to_send: '',
   },
 }
 
