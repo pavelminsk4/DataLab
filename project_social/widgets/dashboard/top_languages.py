b349727b86f7fc9ff104396842d78710e7240ea7
Pavel Hrygoryk <pg@anahorets-iMac.local>
2023-04-27 17:03:38 +0300

    ADD: Add top languages widget to regular report

--- project_social/widgets/dashboard/top_languages.py
+++ project_social/widgets/dashboard/top_languages.py
@@ -5,17 +5,33 @@ from django.db.models.functions import Trunc
 from django.http import JsonResponse
 from django.db.models import Count
 
+
 def post_agregator_top_languages(posts, aggregation_period, top_counts):
-  results = list(posts.annotate(date_trunc=Trunc('date', aggregation_period)).values('language').annotate(language_count=Count('language')).order_by('-language_count')[:top_counts])
-  for res in results:
-    if not res['language']:
-      results.remove(res)
-  return results
+    results = list(posts.annotate(date_trunc=Trunc('date', aggregation_period)).values(
+        'language').annotate(language_count=Count('language')).order_by('-language_count')[:top_counts])
+    for res in results:
+        if not res['language']:
+            results.remove(res)
+    return results
+
+
+def precalculate_result(pk, widget_pk):
+    project = ProjectSocial.objects.get(id=pk)
+    posts = post_agregator_with_dimensions(project)
+    widget = SocialWidgetDescription.objects.get(id=widget_pk)
+    posts = post_agregetor_for_each_widget(widget, posts)
+    return post_agregator_top_languages(posts, widget.aggregation_period, widget.top_counts)
+
 
 def top_languages(pk, widget_pk):
-  project = ProjectSocial.objects.get(id=pk)
-  posts = post_agregator_with_dimensions(project)
-  widget = SocialWidgetDescription.objects.get(id=widget_pk)
-  posts = post_agregetor_for_each_widget(widget, posts)
-  res = post_agregator_top_languages(posts, widget.aggregation_period, widget.top_counts)
-  return JsonResponse(res, safe = False)
+    res = precalculate_result(pk, widget_pk)
+    return JsonResponse(res, safe=False)
+
+
+def top_languages_report(pk, widget_pk):
+    res = precalculate_result(pk, widget_pk)
+    context = reduce(lambda x, y: {
+        'labels': [*x['labels'], y['language']],
+        'data': [*x['data'], y['language_count']]
+    }, res, {'labels': [], 'data': []})
+    return context
