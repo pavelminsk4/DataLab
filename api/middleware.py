55adcd2b34df1e021328e5595c417f1048ba814f
Anzhalika <angelichka21.02@gmail.com>
2023-05-18 15:52:44 +0300

    Fix: Fix  post/put request for mobile app

--- api/middleware.py
+++ api/middleware.py
@@ -1,20 +1,15 @@
-from django.urls import reverse
-import logging
 from django.utils.deprecation import MiddlewareMixin
-# django2
-
 
 class DisableCSRF(MiddlewareMixin):
     """Middleware for disabling CSRF in an specified app name.
     """
 
     def process_request(self, request):
-        """Preprocess the request.
-        """
+        #Preprocess the request.
+        
         logging.error('Your message: ', request.get_full_path())
 
-        app_name = "api"
-        if request.get_full_path().startswith('/api/auth/'):
+        if ("Authorization" in request.headers) or (request.get_full_path().startswith('/api/auth/')):
             setattr(request, '_dont_enforce_csrf_checks', True)
         else:
             pass  # check CSRF token validation
