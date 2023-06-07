c5abbb429b15de6bdaf065e762922a0fa81b61d0
Anzhalika Artsiomenka <angelichka21.02@gmail.com>
2023-06-07 14:32:56 +0300

    ADD: Add(fix) API for Top sources by country

--- widgets/demography/sources_by_country.py
+++ widgets/demography/sources_by_country.py
@@ -9,5 +9,9 @@ def get_sources_by_country(pk, widget_pk):
   posts = post_agregator_with_dimensions(project)
   widget = WidgetDescription.objects.get(id=widget_pk)
   posts = post_agregetor_for_each_widget(widget, posts)
-  posts = list(posts.values('feedlink__country').annotate(source_count=Count('feedlink', distinct=True)).order_by('-source_count')[:widget.top_counts])
-  return JsonResponse(posts, safe = False)
+  top_countries = [i['feedlink__country'] for i in posts.values('feedlink__country').annotate(source_count=Count('feedlink', distinct=True)).order_by('-source_count')[:5]]
+  results =[]
+  for country in top_countries:
+    top_sources = posts.filter(feedlink__country=country).values('feedlink__source1').annotate(posts_count=Count('id')).order_by('-posts_count')[:5]
+    results.append({country: {i['feedlink__source1']:i['posts_count'] for i in top_sources}})
+  return JsonResponse(results, safe = False)
