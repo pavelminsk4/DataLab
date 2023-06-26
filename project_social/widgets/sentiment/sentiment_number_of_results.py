19b9eef09accfbab82abe10289ce91e03843a994
Artur Aliakseyeu <aae@krononsoft.com>
2023-06-26 14:17:29 +0300

    Add sentiment diagram view and template

--- project_social/widgets/sentiment/sentiment_number_of_results.py
+++ project_social/widgets/sentiment/sentiment_number_of_results.py
@@ -13,10 +13,14 @@ def calculate(posts):
     }
 
 
-def sentiment_number_of_results(pk, widget_pk):
+def precalculate_result(pk, widget_pk):
     project = ProjectSocial.objects.get(id=pk)
     posts = post_agregator_with_dimensions(project)
     widget = SocialWidgetDescription.objects.get(id=widget_pk)
     posts = post_agregetor_for_each_widget(widget, posts)
-    res = calculate(posts)
+    return calculate(posts)
+
+
+def sentiment_number_of_results(pk, widget_pk):
+    res = precalculate_result(pk, widget_pk)
     return JsonResponse(res, safe=False)
