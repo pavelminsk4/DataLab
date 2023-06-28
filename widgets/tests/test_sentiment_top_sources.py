4d13d87784a23deb9455c2122896242a70e14523
Artur Aliakseyeu <aae@krononsoft.com>
2023-06-28 12:59:39 +0300

    Refactoring of all tests in online module

--- widgets/tests/test_sentiment_top_sources.py
+++ widgets/tests/test_sentiment_top_sources.py
@@ -1,37 +1,36 @@
+from common.factories.feedlinks import FeedlinksFactory
+from common.factories.project import ProjectFactory
+from common.factories.post import PostFactory
 from rest_framework.test import APITestCase
-from django.urls import reverse
-from datetime import datetime
 from rest_framework import status
+from django.urls import reverse
 import json
-from project.models import Post, Project, Speech, Feedlinks
-from django.contrib.auth.models import User
+
 
 class SentimentTopSourcesTests(APITestCase):
-  def test_response_list(self):
-    user = User.objects.create(username='Pablo')
-    flink3 = Feedlinks.objects.create(country = 'USA', source1='Time')
-    flink4 = Feedlinks.objects.create(country = 'Canada', source1='BBC')
-    sp2 = Speech.objects.create(language='Spain')
-    Post.objects.create(feedlink=flink3, entry_title='5 post title', feed_language=sp2, entry_published=datetime(2023, 9, 3, 6, 37), entry_author='AFP', sentiment='positive', summary_vector=[])
-    Post.objects.create(feedlink=flink3, entry_title='6 post title', feed_language=sp2, entry_published=datetime(2023, 9, 3, 6, 37), entry_author='EFE', sentiment='neutral', summary_vector=[])
-    Post.objects.create(feedlink=flink4, entry_title='5 post title', feed_language=sp2, entry_published=datetime(2023, 9, 3, 6, 37), entry_author='AFP', sentiment='neutral', summary_vector=[])
-    Post.objects.create(feedlink=flink4, entry_title='6 post title', feed_language=sp2, entry_published=datetime(2023, 9, 3, 6, 37), entry_author='', sentiment='neutral', summary_vector=[])
-    # test first project with None field
-    pr1 = Project.objects.create(title='Project1', keywords=['post'], additional_keywords=[], ignore_keywords=[], start_search_date=datetime(2020, 10, 10),
-                                end_search_date=datetime(2023, 10, 16), country_filter='', author_filter='', language_filter='', creator=user)
-    widget_pk = pr1.widgets_list_2.sentiment_top_sources_id
-    url = reverse('widgets:onl_sentiment_top_sources', kwargs={'pk':pr1.pk, 'widget_pk':widget_pk})
-    response = self.client.get(url)
-    self.assertEqual(response.status_code, status.HTTP_200_OK)
-    res = {'BBC': [
-                      {'sentiment': 'neutral', 'sentiment_count': 2},
-                      {'sentiment': 'negative', 'sentiment_count': 0},
-                      {'sentiment': 'positive', 'sentiment_count': 0}
-                    ],
-           'Time': [
-                      {'sentiment': 'neutral', 'sentiment_count': 1},
-                      {'sentiment': 'positive', 'sentiment_count': 1},
-                      {'sentiment': 'negative', 'sentiment_count': 0}
-                    ],
-            }
-    self.assertEqual(json.loads(response.content), res)
+    def test_response_list(self):
+        flink1 = FeedlinksFactory(source1='Time')
+        flink2 = FeedlinksFactory(source1='BBC')
+        PostFactory(feedlink=flink1, sentiment='positive')
+        PostFactory(feedlink=flink1, sentiment='neutral')
+        PostFactory(feedlink=flink2, sentiment='neutral')
+        PostFactory(feedlink=flink2, sentiment='neutral')
+        pr = ProjectFactory()
+
+        widget_pk = pr.widgets_list_2.sentiment_top_sources_id
+        url = reverse('widgets:onl_sentiment_top_sources', kwargs={'pk': pr.pk, 'widget_pk': widget_pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        res = {
+            'BBC': [
+              {'sentiment': 'neutral', 'sentiment_count': 2},
+                {'sentiment': 'negative', 'sentiment_count': 0},
+                {'sentiment': 'positive', 'sentiment_count': 0}
+            ],
+            'Time': [
+                {'sentiment': 'neutral', 'sentiment_count': 1},
+                {'sentiment': 'positive', 'sentiment_count': 1},
+                {'sentiment': 'negative', 'sentiment_count': 0}
+            ],
+        }
+        self.assertEqual(json.loads(response.content), res)
