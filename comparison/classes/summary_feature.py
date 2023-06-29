b73e29597cb95c9c1c5ba729253a2e6e7183a7b4
Anzhalika Artsiomenka <angelichka21.02@gmail.com>
2023-06-29 12:34:43 +0300

    ADD: Add widgets to social reports

--- comparison/classes/summary_feature.py
+++ comparison/classes/summary_feature.py
@@ -3,7 +3,7 @@ from project_social.widgets.dashboard.top_locations import post_agregator_top_lo
 from project_social.widgets.dashboard.top_authors import post_agregator_top_authors as soc_top_authors
 from project_social.widgets.dashboard.summary_widget import calculate_summary_widget as soc_summary
 from project_social.widgets.sentiment.sentiment_number_of_results import calculate as soc_sentiment
-from project_social.widgets.dashboard.content_volume import post_agregator_volume as soc_volume
+from project_social.widgets.dashboard.content_volume import calculate as soc_volume
 from common.social_keywords import get_keywords as soc_top_keywords
 
 from widgets.sentiment.sentiment_number_of_results import get_sentiment_number_of_results as onl_sentiment
--- project_social/widgets/dashboard/content_volume.py
+++ project_social/widgets/dashboard/content_volume.py
@@ -1,12 +1,13 @@
+from project_social.widgets.filters_for_widgets import post_agregator_with_dimensions, post_agregetor_for_each_widget
 from project_social.models import SocialWidgetDescription
-from project_social.widgets.filters_for_widgets import *
 from project_social.models import ProjectSocial
+from django.forms.models import model_to_dict
 from django.db.models.functions import Trunc
 from django.http import JsonResponse
 from django.db.models import Count
 import json
 
-def post_agregator_volume(posts, aggregation_period):
+def calculate(posts, aggregation_period):
   posts = posts.annotate(date_trunc=Trunc('date', aggregation_period)).values("date_trunc").annotate(created_count=Count('id')).order_by("date")
   dates = set()
   for elem in range(len(posts)):
@@ -27,5 +28,15 @@ def content_volume(request, pk, widget_pk):
   posts = post_agregetor_for_each_widget(widget, posts)
   body = json.loads(request.body)
   aggregation_period = body['aggregation_period']
-  res = post_agregator_volume(posts, aggregation_period)
+  res = calculate(posts, aggregation_period)
   return JsonResponse(res, safe = False)
+
+def content_volume_report(pk, widget_pk):
+    project = ProjectSocial.objects.get(id=pk)
+    posts = post_agregator_with_dimensions(project)
+    widget = SocialWidgetDescription.objects.get(id=widget_pk)
+    posts = post_agregetor_for_each_widget(widget, posts)
+    return {
+        'data': calculate(posts, widget.aggregation_period),
+        'widget': {'content_volume_top_locations': model_to_dict(widget)}
+    }
