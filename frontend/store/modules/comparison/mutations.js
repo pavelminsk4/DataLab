eb0e548fad08f7c1635ddb2947728644d300d73a
Falderian <nikitastaselovich@gmail.com>
2023-07-06 14:45:34 +0300

    FIX: fixed data reset for features

--- frontend/store/modules/comparison/mutations.js
+++ frontend/store/modules/comparison/mutations.js
@@ -41,4 +41,8 @@ export default {
   [mutator.SET_LOADING](state, loading) {
     state.loading = loading
   },
+
+  [mutator.CLEAR_WIDGETS_DATA](state) {
+    Object.values(state.widgets).forEach((feature) => (feature.widgets = []))
+  },
 }
