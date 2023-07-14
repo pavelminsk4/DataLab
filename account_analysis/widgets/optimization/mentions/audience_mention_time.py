23791d540ec839e65bfd79f6033d1fe0c8904609
Anzhalika Artsiomenka <angelichka21.02@gmail.com>
2023-07-14 15:22:30 +0300

    FIX: Fix average engagements by day

--- account_analysis/widgets/optimization/mentions/audience_mention_time.py
+++ account_analysis/widgets/optimization/mentions/audience_mention_time.py
@@ -1,9 +1,7 @@
 from account_analysis.widgets.optimization.optimal_post_time import calculation
-from account_analysis.widgets.filter_for_posts import posts_aggregator
+from account_analysis.widgets.filter_for_posts import filter_for_mentions_posts
 from account_analysis.models import ProjectAccountAnalysis
 
 def audience_mention_time(pk, widget_pk):
-    project = ProjectAccountAnalysis.objects.get(id=pk)
-    posts = posts_aggregator(project)
-    posts = posts.filter(text__icontains=f'@{project.profile_handle}')
+    posts, project = filter_for_mentions_posts(pk, widget_pk)
     return calculation(posts)
--- account_analysis/widgets/optimization/mentions/average_engagements_by_day_for_mentions.py
+++ account_analysis/widgets/optimization/mentions/average_engagements_by_day_for_mentions.py
@@ -1,9 +1,8 @@
 from account_analysis.widgets.optimization.average_engagements_by_day import calculate
-from account_analysis.widgets.filter_for_posts import posts_aggregator
+from account_analysis.widgets.filter_for_posts import filter_for_mentions_posts
 from account_analysis.models import ProjectAccountAnalysis
 
 def average_engagements_by_day_for_mentions(pk, widget_pk):
     project = ProjectAccountAnalysis.objects.get(id=pk)
-    posts = posts_aggregator(project)
-    posts = posts.filter(text__icontains=f'@{project.profile_handle}')
+    posts, project = filter_for_mentions_posts(pk, widget_pk)
     return calculate(posts)
