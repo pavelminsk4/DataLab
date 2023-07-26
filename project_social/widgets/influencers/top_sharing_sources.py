cfff9e6a5ce3f4f211d9dc1235785a5d667c6808
Anzhalika Artsiomenka <angelichka21.02@gmail.com>
2023-07-26 10:41:45 +0300

    FIX: Fix top sharing top sources

--- project_social/widgets/influencers/top_sharing_sources.py
+++ project_social/widgets/influencers/top_sharing_sources.py
@@ -17,8 +17,8 @@ def top_sharing_sources_report(pk, widget_pk):
     }
 
 def get_mosts(posts):
-  most_active_author = posts.annotate(author_count=Count("user_alias")).order_by("-author_count").first()
-  most_active_author_posts = posts.filter(user_alias=most_active_author.user_alias)
+  most_active_author = posts.values('user_alias', 'user_name', 'user_picture', 'user_gender').order_by('-user_alias__count').annotate(Count('user_alias')).first()
+  most_active_author_posts = posts.filter(user_alias=most_active_author['user_alias'])
 
   most_influental_author_posts = posts.annotate(engagements=Sum(F("count_totalretweets") + F("count_favorites"))).order_by("-engagements")
   most_influental_author_posts = most_influental_author_posts.filter(user_alias=most_influental_author_posts.first().user_alias)
@@ -27,16 +27,16 @@ def get_mosts(posts):
   return [
     {
       "type": "Most active author",
-      "name": most_active_author.user_name,
-      "alias": most_active_author.user_alias,
+      "name": most_active_author['user_name'],
+      "alias": most_active_author['user_alias'],
       "value": f"{most_active_author_posts.count()} post",
       "sentiments": {
         "positive": most_active_author_posts.filter(sentiment="positive").count(),
         "negative": most_active_author_posts.filter(sentiment="negative").count(),
         "neutral": most_active_author_posts.filter(sentiment="neutral").count(),
       },
-      "picture": most_active_author.user_picture,
-      "gender": most_active_author.user_gender,
+      "picture": most_active_author['user_picture'],
+      "gender": most_active_author['user_gender'],
       "source": "Twitter",
     },
     {
