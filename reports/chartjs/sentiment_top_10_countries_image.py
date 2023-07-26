c13114be5ccea447ecf0c56866136f12a94f8934
Anzhalika Artsiomenka <angelichka21.02@gmail.com>
2023-07-26 16:06:30 +0300

    Fix:  Fix online report

--- reports/chartjs/sentiment_top_10_countries_image.py
+++ reports/chartjs/sentiment_top_10_countries_image.py
@@ -1,14 +1,12 @@
-from widgets.common_widget.sentiment_top_countries import *
-from project.models import *
-from quickchart import QuickChart
+
 from .services.algorithm_for_count_sentiment_top_10_widgets import algorithm_for_count_sentiment_top_10_widgets
+from widgets.common_widget.project_posts_filter import project_posts_filter
+from django.db.models import Count
+
 
 def create_sentiment_top_10_countries_wid_image(project_id, widget_pk):
-  proj = Project.objects.get(id=project_id)
-  posts = post_agregator_with_dimensions(proj)
-  widget = WidgetDescription.objects.get(id=widget_pk)
-  posts = post_agregetor_for_each_widget(widget, posts)
-  top_countries = posts.values('feedlink__country').annotate(brand_count=Count('feedlink__country')).order_by('-brand_count').values_list('feedlink__country', flat=True)[:10]
-  results = {country: list(posts.filter(feedlink__country=country).values('sentiment').annotate(sentiment_count=Count('sentiment')).order_by('-sentiment_count')) for country in top_countries}
-  qc = algorithm_for_count_sentiment_top_10_widgets(results, top_countries)
-  qc.to_file('tmp/sentiment_top_10_countries_widget.png')
+    posts, widget = project_posts_filter(project_id, widget_pk)
+    top_countries = posts.values('feedlink__country').annotate(brand_count=Count('feedlink__country')).order_by('-brand_count').values_list('feedlink__country', flat=True)[:10]
+    results = {country: list(posts.filter(feedlink__country=country).values('sentiment').annotate(sentiment_count=Count('sentiment')).order_by('-sentiment_count')) for country in top_countries}
+    qc = algorithm_for_count_sentiment_top_10_widgets(results, top_countries)
+    qc.to_file('tmp/sentiment_top_10_countries_widget.png')
--- reports/chartjs/sentiment_top_10_sources_widget_image.py
+++ reports/chartjs/sentiment_top_10_sources_widget_image.py
@@ -1,14 +1,11 @@
-from widgets.common_widget.sentiment_top_sources import *
-from project.models import *
-from quickchart import QuickChart
 from .services.algorithm_for_count_sentiment_top_10_widgets import algorithm_for_count_sentiment_top_10_widgets
+from widgets.common_widget.project_posts_filter import project_posts_filter
+from django.db.models import Count
+
 
 def create_sentiment_top_10_sources_wid_image(project_id, widget_pk):
-  proj = Project.objects.get(id=project_id)
-  posts = post_agregator_with_dimensions(proj)
-  widget = WidgetDescription.objects.get(id=widget_pk)
-  posts = post_agregetor_for_each_widget(widget, posts)
-  top_sources = posts.values('feedlink__source1').annotate(brand_count=Count('feedlink__source1')).order_by('-brand_count').values_list('feedlink__source1', flat=True)[:10]
-  results = {source: list(posts.filter(feedlink__source1=source).values('sentiment').annotate(sentiment_count=Count('sentiment')).order_by('-sentiment_count')) for source in top_sources}
-  qc = algorithm_for_count_sentiment_top_10_widgets(results, top_sources)
-  qc.to_file('tmp/sentiment_top_10_sources_widget.png')
+    posts, widget = project_posts_filter(project_id, widget_pk)
+    top_sources = posts.values('feedlink__source1').annotate(brand_count=Count('feedlink__source1')).order_by('-brand_count').values_list('feedlink__source1', flat=True)[:10]
+    results = {source: list(posts.filter(feedlink__source1=source).values('sentiment').annotate(sentiment_count=Count('sentiment')).order_by('-sentiment_count')) for source in top_sources}
+    qc = algorithm_for_count_sentiment_top_10_widgets(results, top_sources)
+    qc.to_file('tmp/sentiment_top_10_sources_widget.png')
