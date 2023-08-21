280db3144b7d2f2ffad48ea14ea1450b90ff0a7d
Pavel Hrygoryk <pg@anahorets-iMac.local>
2023-08-21 11:18:07 +0300

    ADD: Add import export to the multilingual module

--- multilanguage/admin.py
+++ multilanguage/admin.py
@@ -1,7 +1,18 @@
-from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
 from multilanguage.models import PhraseTranslations
+from import_export import resources
+from django.contrib import admin
 
 
-@admin.register(PhraseTranslations)
-class PhraseTranslationsAdmin(admin.ModelAdmin):
+class PhraseTranslationsResource(resources.ModelResource):
+    class Meta:
+        model = PhraseTranslations
+
+
+class PhraseTranslationsAdmin(ImportExportModelAdmin):
+    resource_class = PhraseTranslationsResource
     list_display = ('en', 'ar', 'updated_at', 'created_at')
+    search_fields = ('en', 'ar')
+
+
+admin.site.register(PhraseTranslations, PhraseTranslationsAdmin)
