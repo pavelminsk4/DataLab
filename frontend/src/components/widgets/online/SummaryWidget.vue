4122ce53936ace2fe224f53a3c3581892e0a0e5b
dpk <dpk@anadeainc.com>
2023-09-04 11:06:34 +0300

    FIX: summary bug

--- frontend/src/components/widgets/online/SummaryWidget.vue
+++ frontend/src/components/widgets/online/SummaryWidget.vue
@@ -3,14 +3,14 @@
     v-bind="$attrs"
     :widget-details="widgetDetails"
     :widget-metrics="widgetMetrics"
-    :summary-widget-data="summary"
+    :summary-widget-data="summaryData"
   />
 </template>
 
 <script>
 import {mapActions, mapGetters} from 'vuex'
 import {action, get} from '@store/constants'
-// import {isAllFieldsEmpty} from '@lib/utilities'
+import {isAllFieldsEmpty} from '@lib/utilities'
 import {summaryWidgetConfig} from '@/lib/configs/widgetsConfigs'
 
 import SummaryWidget from '@/components/widgets/SummaryWidget'
@@ -26,16 +26,25 @@ export default {
       availableWidgets: get.AVAILABLE_WIDGETS,
       summary: get.SUMMARY_WIDGET,
     }),
+    summaryData() {
+      return this.widgetDetails.widgetData || this.summary.data
+    },
+    widgetId() {
+      return this.summary?.id
+    },
   },
   created() {
     this.widgetMetrics = summaryWidgetConfig
 
-    // if (isAllFieldsEmpty(this.summary)) {
-    this[action.GET_SUMMARY_WIDGET]({
-      projectId: this.widgetDetails.projectId,
-      widgetId: this.widgetDetails.id,
-    })
-    // }
+    const hasCurrentData =
+      !isAllFieldsEmpty(this.summary) && this.widgetId === this.widgetDetails.id
+
+    if (!this.widgetDetails.widgetData && !hasCurrentData) {
+      this[action.GET_SUMMARY_WIDGET]({
+        projectId: this.widgetDetails.projectId,
+        widgetId: this.widgetDetails.id,
+      })
+    }
   },
   methods: {
     ...mapActions([action.GET_SUMMARY_WIDGET]),
