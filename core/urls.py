a157720e51bb38f1b26a275fbbe82d95228028e6
Anzhalika <angelichka21.02@gmail.com>
2023-09-06 14:58:18 +0300

    ADD:  Fix landing en page and add ar page

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.home, name='home'),
+    path('home/ar/', views.home_ar, name='home_ar'),
     path('home/contact/', views.contact, name='contact'),
+    path('home/contact_ar/', views.contact_ar, name='contact_ar'),
 ]
