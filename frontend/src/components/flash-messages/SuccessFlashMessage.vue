b0a7472eabab7a5ed4b24c06b02cf412e06b0460
Dmitry Shvabovich <dgs@krononsoft.com>
2023-09-18 17:48:43 +0300

    ADD: notification of instant report

---
+++ frontend/src/components/flash-messages/SuccessFlashMessage.vue
@@ -0,0 +1,21 @@
+<template>
+  <BaseFlashMessage v-bind="$attrs" class="error-flash-message">
+    <slot></slot>
+  </BaseFlashMessage>
+</template>
+
+<script>
+import BaseFlashMessage from '@components/flash-messages/BaseFlashMessage'
+
+export default {
+  name: 'SuccessFlashMessage',
+  components: {BaseFlashMessage},
+}
+</script>
+
+<style lang="scss" scoped>
+.error-flash-message {
+  border-color: var(--positive-primary-color);
+  background-color: var(--positive-secondary-color);
+}
+</style>
--- frontend/src/components/project/dashboard/OnlineProjectDashboard.vue
+++ frontend/src/components/project/dashboard/OnlineProjectDashboard.vue
@@ -237,6 +237,7 @@ export default {
     ...mapActions([
       action.UPDATE_ADDITIONAL_FILTERS,
       action.CLEAR_INTERACTIVE_DATA,
+      action.OPEN_FLASH_MESSAGE,
     ]),
     ...mapOnlineActions([
       action.POST_SEARCH,
@@ -319,6 +320,12 @@ export default {
           this.toggleWidgetsModal('isOpenDownloadReportModal')
           const res = await this[action.GET_INSTANT_REPORT]({
             projectId: this.currentProject.id,
+          })
+
+          this[action.OPEN_FLASH_MESSAGE]({
+            message: 'Your report is generated. Starting the download.',
+            title: 'Instant Report',
+            type: 'Success',
           })
 
           const anchor = document.createElement('a')
