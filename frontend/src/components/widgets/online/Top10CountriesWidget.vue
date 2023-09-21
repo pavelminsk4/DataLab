1bb029dd5600a55a9617d9792918eaaee50c9fa9
Dmitry Shvabovich <dgs@krononsoft.com>
2023-09-21 15:56:30 +0300

    UPDATE: search screen

--- frontend/src/components/widgets/online/Top10CountriesWidget.vue
+++ frontend/src/components/widgets/online/Top10CountriesWidget.vue
@@ -49,7 +49,6 @@ export default {
       this.topCountries.length && this.widgetId === this.widgetDetails.id
 
     if (!this.widgetDetails.widgetData && !hasCurrentData) {
-      console.log('????')
       this[action.GET_TOP_COUNTRIES_WIDGET]({
         projectId: this.widgetDetails.projectId,
         widgetId: this.widgetDetails.id,
