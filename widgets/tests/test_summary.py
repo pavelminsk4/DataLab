f50d3c16d9a12758c60ac0d62fc0282d6a27e407
Anzhalika Artsiomenka <angelichka21.02@gmail.com>
2023-10-05 15:48:15 +0300

    FIX: Fix summary widget

---
+++ widgets/tests/test_summary.py
@@ -0,0 +1,47 @@
+from common.factories.talkwalker_post import TalkwalkerPostFactory
+from common.factories.project import ProjectFactory
+from common.factories.post import PostFactory
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+import json, os
+
+
+class SummaryTests(APITestCase):
+    def setUp(self):
+        os.environ['POST_LOCATOR'] = 'rss'
+
+    def test_response_list(self):
+        p1 = PostFactory.create_batch(4, entry_author='AFP')
+        p2 = PostFactory.create_batch(2, entry_author='EFE')
+        pr = ProjectFactory()
+        for p in p1:
+            pr.posts.add(p)
+        for p in p2:
+            pr.posts.add(p)
+        widget_pk = pr.widgets_list_2.summary_id
+        url = reverse('widgets:onl_summary', kwargs={'pk': pr.pk, 'widget_pk': widget_pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        res = {'posts': 6, 'sources': 1, 'authors': 1, 'countries': 1, 'languages': 1, 'positive': 0, 'negative': 0, 'neutral': 6, 'reach': 0}
+        self.assertEqual(json.loads(response.content), res)
+
+
+class SummaryTestsTLW(APITestCase):
+    def setUp(self):
+        os.environ['POST_LOCATOR'] = 'talkwalker'
+
+    def test_response_list_tlw(self):
+        p1 = TalkwalkerPostFactory.create_batch(4, entry_author='AFP')
+        p2 = TalkwalkerPostFactory.create_batch(2, entry_author='EFE')
+        pr = ProjectFactory()
+        for p in p1:
+            pr.tw_posts.add(p)
+        for p in p2:
+            pr.tw_posts.add(p)
+        widget_pk = pr.widgets_list_2.summary_id
+        url = reverse('widgets:onl_summary', kwargs={'pk': pr.pk, 'widget_pk': widget_pk})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        res = {'posts': 6, 'sources': 1, 'authors': 1, 'countries': 1, 'languages': 1, 'positive': 0, 'negative': 0, 'neutral': 6, 'reach': 0}
+        self.assertEqual(json.loads(response.content), res)
