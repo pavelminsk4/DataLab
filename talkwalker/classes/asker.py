a879143c9891c0f629684dbdbc96e9ca4deb1945
Vadim Kondratiev <vvk@anadeainc.com>
2023-10-26 15:35:20 +0300

    Read Talkwalker streams in chunks

--- talkwalker/classes/asker.py
+++ talkwalker/classes/asker.py
@@ -11,52 +11,57 @@ import json
 
 
 class Asker:
+    task_id = ''
+    token   = get_token()
+
     def __init__(self, project_id, module):
         model = ''
-        if module=='Project':
+        if module == 'Project':
             model = apps.get_model('project', 'Project')
             self.collector_id = f'search-{project_id}-onl-col'
-        if module=='ProjectTwentyFourSeven':
+        if module == 'ProjectTwentyFourSeven':
             model = apps.get_model('twenty_four_seven', 'ProjectTwentyFourSeven')
             self.collector_id = f'search-{project_id}-tfs-col'
         self.project = model.objects.get(id=project_id)
 
-    token = get_token()
-    task_id = ''
-
     def __01_create_target_collector(self):
         url = f'https://api.talkwalker.com/api/v3/stream/c/{self.collector_id}?access_token={self.token}'
-        headers = { 'Content-Type': 'application/json' }
+
+        headers  = {'Content-Type': 'application/json'}
         response = requests.request('PUT', url, headers=headers, data={})
+
         return response.status_code == status.HTTP_200_OK
 
     def __02_new_task_on_query(self):
         url = f'https://api.talkwalker.com/api/v3/stream/export?access_token={self.token}'
+
         payload = json.dumps({
             'target': self.collector_id,
-            'start':  self.project.start_search_date.date().isoformat(),
-            'stop':   self.project.end_search_date.date().isoformat(),
-            'limit':  environ.Env()('TALKWALKER_LIMIT'),
-            'query':  get_tw_query(self.project),
+            'start': self.project.start_search_date.date().isoformat(),
+            'stop': self.project.end_search_date.date().isoformat(),
+            'limit': environ.Env()('TALKWALKER_LIMIT'),
+            'query': get_tw_query(self.project),
         })
-        headers = { 'Content-Type': 'application/json' }
+
+        headers  = {'Content-Type': 'application/json'}
         response = requests.request('POST', url, headers=headers, data=payload)
-        lines = response.iter_lines()
+        lines    = response.iter_lines()
+
         for line in lines:
             self.task_id = json.loads(line)['result_tasks']['tasks'][0]['id']
         return response.status_code == status.HTTP_200_OK
 
     def __03_retrieve_status_of_task(self):
         url = f'https://api.talkwalker.com/api/v3/tasks/export/{self.task_id}?access_token={self.token}'
+
         response = requests.request('GET', url, headers={}, data={})
-        lines = response.iter_lines()
+        lines    = response.iter_lines()
+
         for line in lines:
             try:
                 status = json.loads(line)['result_tasks']['tasks'][0]['status']
-                print(status)
             except:
                 status = ''
-                pass
         return status
 
     def __wait_until_limit_reached(self):
@@ -67,12 +72,20 @@ class Asker:
             if status == 'result_limit_reached':
                 break
 
-    def __04_read_collector(self):
-        url = f'https://api.talkwalker.com/api/v3/stream/c/{self.collector_id}/results?access_token={self.token}&resume_offset=earliest&end_behaviour=stop'
+    def __read_chunk(self, offset):
+        url = f'https://api.talkwalker.com/api/v3/stream/c/{self.collector_id}/results?access_token={self.token}&resume_offset={offset}&end_behaviour=stop'
+
         response = requests.request('GET', url, headers={}, data={})
-        lines = response.iter_lines()
-        create_posts(self.project, lines)
-        return response.status_code == status.HTTP_200_OK
+        lines    = response.iter_lines()
+
+        return create_posts(self.project, lines, offset)
+
+    def __04_read_collector(self):
+        resume_offset = 'earliest'
+        while resume_offset:
+            resume_offset = self.__read_chunk(resume_offset)
+
+        return True
 
     def __05_delete_collector(self):
         url = f'https://api.talkwalker.com/api/v3/stream/c/{self.collector_id}?access_token={self.token}'
---
+++ talkwalker/tests/test_collector_read.py
@@ -0,0 +1,49 @@
+from common.factories.project import ProjectFactory
+from talkwalker.classes.asker import Asker
+from project.models import Post
+
+from django.test import TestCase
+from django.conf import settings
+
+import responses
+import os
+import re
+import json
+
+
+class AskerTestCase(TestCase):
+    def read_file(self, name):
+        return '}\n{'.join(re.sub(r'\s+', '', open(os.path.join(settings.BASE_DIR, name)).read()).split('}{'))
+
+    @responses.activate
+    def test_collector_read(self):
+        """Collector reads posts through all chunks"""
+        project = ProjectFactory()
+
+        responses.add(
+            responses.GET,
+            f'https://api.talkwalker.com/api/v3/stream/c/search-{project.id}-onl-col/results?access_token=acfaad27-3948-4e13-a617-2d33fd97552a_ZStqaRbxfzpRBT7YfbIqUPJPVwa2.QUzpFqzvPtKdHdc5UNZrYlHyk03MPxVkGKfZ6n3Y4i.meiy.FNZ621ZSmhzzb.gJAlvHtslEq7PP0B3JbDC4BS1y.hwK9fPz7vMQXvSfQFXka9.gEKovum-Pu0LYzy8GSjjSLvBdA1fV5M&resume_offset=earliest&end_behaviour=stop',
+            body=self.read_file('fixtures/talkwalker/collector_stream1.json')
+        )
+
+        responses.add(
+            responses.GET,
+            f'https://api.talkwalker.com/api/v3/stream/c/search-{project.id}-onl-col/results?access_token=acfaad27-3948-4e13-a617-2d33fd97552a_ZStqaRbxfzpRBT7YfbIqUPJPVwa2.QUzpFqzvPtKdHdc5UNZrYlHyk03MPxVkGKfZ6n3Y4i.meiy.FNZ621ZSmhzzb.gJAlvHtslEq7PP0B3JbDC4BS1y.hwK9fPz7vMQXvSfQFXka9.gEKovum-Pu0LYzy8GSjjSLvBdA1fV5M&resume_offset=EgT4kpRi&end_behaviour=stop',
+            body=self.read_file('fixtures/talkwalker/collector_stream2.json')
+        )
+
+        responses.add(
+            responses.GET,
+            f'https://api.talkwalker.com/api/v3/stream/c/search-{project.id}-onl-col/results?access_token=acfaad27-3948-4e13-a617-2d33fd97552a_ZStqaRbxfzpRBT7YfbIqUPJPVwa2.QUzpFqzvPtKdHdc5UNZrYlHyk03MPxVkGKfZ6n3Y4i.meiy.FNZ621ZSmhzzb.gJAlvHtslEq7PP0B3JbDC4BS1y.hwK9fPz7vMQXvSfQFXka9.gEKovum-Pu0LYzy8GSjjSLvBdA1fV5M&resume_offset=EgSIlpRi&end_behaviour=stop',
+            body=self.read_file('fixtures/talkwalker/collector_stream3.json')
+        )
+
+        responses.add(
+            responses.GET,
+            f'https://api.talkwalker.com/api/v3/stream/c/search-{project.id}-onl-col/results?access_token=acfaad27-3948-4e13-a617-2d33fd97552a_ZStqaRbxfzpRBT7YfbIqUPJPVwa2.QUzpFqzvPtKdHdc5UNZrYlHyk03MPxVkGKfZ6n3Y4i.meiy.FNZ621ZSmhzzb.gJAlvHtslEq7PP0B3JbDC4BS1y.hwK9fPz7vMQXvSfQFXka9.gEKovum-Pu0LYzy8GSjjSLvBdA1fV5M&resume_offset=EgTslpRi&end_behaviour=stop',
+            body=self.read_file('fixtures/talkwalker/collector_stream4.json')
+        )
+
+        result = Asker(project.id, 'Project')._Asker__04_read_collector()
+        self.assertTrue(result)
+        self.assertTrue(project.posts.all().count() > 328)
