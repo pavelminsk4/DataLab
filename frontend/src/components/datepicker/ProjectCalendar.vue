1b9226a881ffe4ab79bcd7f30587767f36ae09c0
dpk <dpk@anadeainc.com>
2023-11-03 15:52:28 +0300

    fix project calendar

--- frontend/src/components/datepicker/ProjectCalendar.vue
+++ frontend/src/components/datepicker/ProjectCalendar.vue
@@ -6,7 +6,7 @@
       placeholder="Select date"
       text-input
       :range="isRange"
-      :disabled-dates="disabledAfterToday"
+      :disabled-dates="disabledDates"
     />
   </div>
 </template>
@@ -25,6 +25,7 @@ export default {
   props: {
     isDesableAfterToday: {type: Boolean, default: true},
     isRange: {type: Boolean, default: false},
+    isCurrentProject: {type: Boolean, default: false},
   },
   data() {
     return {
@@ -33,9 +34,12 @@ export default {
   },
   computed: {
     ...mapGetters({additionalFilters: get.ADDITIONAL_FILTERS}),
+    initialDate() {
+      return this.additionalFilters?.date_range[0]
+    },
     selectedDateProxy: {
       get() {
-        return this.date
+        return this.isRange ? [this.initialDate, new Date()] : this.date
       },
       set(newValue) {
         this.date = newValue
@@ -47,9 +51,13 @@ export default {
   },
   methods: {
     ...mapActions([action.UPDATE_ADDITIONAL_FILTERS]),
-    disabledAfterToday(date) {
+    disabledDates(date) {
+      var startDate = new Date(this.initialDate).setDate(
+        this.initialDate.getDate() - 1
+      )
+
       return this.isRange && this.isDesableAfterToday
-        ? date < this.additionalFilters.date_range[0]
+        ? date < new Date(startDate)
         : date > new Date()
     },
   },
