cdba31b126c48672cf609eb53f3879990b06969c
Pavel Hrygoryk <pg@anahorets-iMac.local>
2023-11-13 15:46:18 +0300

    Implement expert_filters backend

--- expert_filters/models.py
+++ expert_filters/models.py
@@ -1,19 +1,20 @@
+from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
 from django.db import models
 
 
-class Group(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=1000, null=True, blank=True)
-    creator = models.ForeignKey(User, related_name='group_creator', on_delete=models.SET_NULL, null=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-
 class Preset(models.Model):
-    title = models.CharField(max_length=100)
-    query = models.TextField()
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True)
-    creator = models.ForeignKey(User, related_name='preset_creator', on_delete=models.SET_NULL, null=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+    title       = models.CharField(max_length=100)
+    query       = ArrayField(models.TextField(), null=True, blank=True)
+    creator     = models.ForeignKey(User, related_name='preset_creator', on_delete=models.SET_NULL, null=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+    created_at  = models.DateTimeField(auto_now_add=True)
+
+
+class Group(models.Model):
+    title       = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000, null=True, blank=True)
+    presets     = models.ManyToManyField(Preset, blank=True)
+    creator     = models.ForeignKey(User, related_name='group_creator', on_delete=models.SET_NULL, null=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+    created_at  = models.DateTimeField(auto_now_add=True)
--- expert_filters/views.py
+++ expert_filters/views.py
@@ -1,4 +1,6 @@
-from expert_filters.serializers import GroupSerializer, PresetSerializer
+from expert_filters.serializers import GroupNestedSerializer
+from expert_filters.serializers import GroupBasicSerializer
+from expert_filters.serializers import PresetSerializer
 from expert_filters.models import Group, Preset
 from rest_framework import viewsets
 
@@ -11,16 +13,16 @@ class GroupsViewSet(viewsets.ModelViewSet):
         return Group.objects.none()
 
     def get_serializer_class(self):
-        return GroupSerializer
+        if self.request.method == 'GET':
+            return GroupNestedSerializer
+        return GroupBasicSerializer
 
 
 class PresetsViewSet(viewsets.ModelViewSet):
+    serializer_class = PresetSerializer
+
     def get_queryset(self):
-        group = self.kwargs['group_pk']
         user = self.request.user
         if not user.is_anonymous:
-            return Preset.objects.filter(creator=user).filter(group=group)
+            return Preset.objects.filter(creator=user)
         return Preset.objects.none()
-
-    def get_serializer_class(self):
-        return PresetSerializer
