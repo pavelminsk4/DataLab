51fcf9df19a9b593ea95fde133d4784d6566c567
dpk <dpk@anadeainc.com>
2023-11-30 17:08:28 +0300

    fix online

--- frontend/src/components/project/search/OnlineSearch.vue
+++ frontend/src/components/project/search/OnlineSearch.vue
@@ -68,6 +68,7 @@ export default {
     ]),
     ...mapOnlineActions([
       action.POST_SEARCH,
+      action.GET_WORKSPACES,
       action.UPDATE_PROJECT,
       action.GET_AVAILABLE_WIDGETS,
     ]),
@@ -87,6 +88,8 @@ export default {
         })
 
         this[action.CLEAR_STATE]()
+
+        this[action.GET_WORKSPACES]()
       } catch (e) {
         console.error(e)
       }
