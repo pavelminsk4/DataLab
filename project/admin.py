06a948591d8089c9973a25e6460b58a4a45db362
Vadim Kondratiev <vvk@anadeainc.com>
2023-12-15 14:51:56 +0300

    Add country to speeches admin

--- project/admin.py
+++ project/admin.py
@@ -5,78 +5,90 @@ from import_export import resources
 
 
 admin.site.register(Workspace)
-admin.site.register(Speech)
 admin.site.register(Status)
+
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'keywords', 'creator', 'created_at')
     exclude = ('posts',)
 
+
 def make_approved(modeladmin, request, queryset):
     queryset.update(is_approved=True)
-make_approved.short_description = "Mark selected feeds as approved."
+
+make_approved.short_description = 'Mark selected feeds as approved.'
+
 
 def move_approved(modeladmin, request, queryset):
-  for link in queryset:
-    if link.is_approved:
-      feed, created = Feedlinks.objects.get_or_create(
-        url = link.url
-      )
-      feed.source1 = link.source1
-      feed.sourceurl = link.sourceurl
-      feed.county = link.country
-      feed.save()
-      link.delete()
-move_approved.short_description = "Move the selected feeds to the common scope of Feedlinks."
+    for link in queryset:
+        if link.is_approved:
+            feed, created = Feedlinks.objects.get_or_create(url=link.url)
+            feed.source1 = link.source1
+            feed.sourceurl = link.sourceurl
+            feed.county = link.country
+            feed.save()
+            link.delete()
+
+move_approved.short_description = 'Move the selected feeds to the common scope of Feedlinks.'
+
+
+@admin.register(Speech)
+class SpeechesAdmin(admin.ModelAdmin):
+    list_display = ('language', 'country')
+
 
 @admin.register(NewFeedlinks)
 class NewFeedlinksAdmin(admin.ModelAdmin):
     list_display = ('source1', 'url', 'sourceurl', 'country', 'is_approved', 'created_at', 'updated_at')
     search_fields = ('source1__startswith', 'url__startswith', 'sourceurl__startswith')
     actions = [make_approved, move_approved]
-    
+
+
 @admin.register(CrawlerKeyword)
 class CrawlerKeywordAdmin(admin.ModelAdmin):
     list_display = ('word', 'created_at', 'updated_at')
+
 
 @admin.register(CrawlerOption)
 class CrawlerOptionAdmin(admin.ModelAdmin):
     list_display = ('location', 'tbm', 'gl', 'safe', 'is_active', 'created_at', 'updated_at')
 
-# === Feedlinks Import-Export
-class FeedlinksResource(resources.ModelResource):
-  class Meta:
-    model = Feedlinks
 
-  def skip_row(self, instance, original):
-    return Feedlinks.objects.filter(url=instance.url).exists()
+class FeedlinksResource(resources.ModelResource):
+    class Meta:
+        model = Feedlinks
+
+    def skip_row(self, instance, original):
+        return Feedlinks.objects.filter(url=instance.url).exists()
+
 
 class FeedlinksAdmin(ImportExportModelAdmin):
-  resource_class = FeedlinksResource
-  list_display = ('source1', 'url', 'country')
-  search_fields = ('source1__startswith', 'url__startswith')
+    resource_class = FeedlinksResource
+    list_display = ('source1', 'url', 'country')
+    search_fields = ('source1__startswith', 'url__startswith')
 
-admin.site.register(Feedlinks, FeedlinksAdmin )
+admin.site.register(Feedlinks, FeedlinksAdmin)
 
-# ==== Post Import-Export
+
 class PostResource(resources.ModelResource):
-  class Meta:
-    model = Post
+    class Meta:
+        model = Post
+
 
 class PostAdmin(ImportExportModelAdmin):
-  resource_class = PostResource
-  list_display = ('entry_title', 'creationdate')
+    resource_class = PostResource
+    list_display = ('entry_title', 'creationdate')
 
 admin.site.register(Post, PostAdmin)
 
-# ==== TempFeedLinks ===
 
 class TempFeedLinksResource(resources.ModelResource):
-  class Meta:
-    model = TempFeedLinks
+    class Meta:
+        model = TempFeedLinks
+
 
 class TempFeedLinksAdmin(ImportExportModelAdmin):
-  resource_class = TempFeedLinksResource
+    resource_class = TempFeedLinksResource
 
 admin.site.register(TempFeedLinks, TempFeedLinksAdmin)
