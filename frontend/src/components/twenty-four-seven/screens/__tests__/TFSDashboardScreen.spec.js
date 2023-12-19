09dfdc164cd602d35444105b10b774ab63c5d6d7
dpk <dpk@anadeainc.com>
2023-12-19 11:14:47 +0300

    add test for tfs dashboard screen

--- frontend/src/components/twenty-four-seven/screens/__tests__/TFSDashboardScreen.spec.js
+++ frontend/src/components/twenty-four-seven/screens/__tests__/TFSDashboardScreen.spec.js
@@ -4,6 +4,8 @@ import router from '@router/__mocks__/router'
 import {createNewStore, mockmixin} from '@lib/test-helpers'
 import {dragAndDropStatuses} from '@lib/configs/tfsStatusesConfig'
 
+import TFSPostCard from '@components/TFSPostCard'
+import TFSWorkingModal from '@components/twenty-four-seven/modals/TFSWorkingModal'
 import TFSDragAndDrop from '@components/twenty-four-seven/drag-n-drop/TFSDragAndDrop'
 import TFSDashboardScreen from '@components/twenty-four-seven/screens/TFSDashboardScreen'
 
@@ -17,6 +19,10 @@ const createWrapper = (store) =>
             workspaceId: 2,
             projectId: 7,
           },
+          query: {
+            modal: 'Working',
+            tab: 'Original content',
+          },
         },
         $router: {
           push: jest.fn(),
@@ -26,6 +32,7 @@ const createWrapper = (store) =>
       mixins: [mockmixin],
       data() {
         return {
+          postInfo: null,
           currentStatuses: dragAndDropStatuses,
         }
       },
@@ -36,7 +43,7 @@ const createWrapper = (store) =>
   })
 
 describe('TFSDashboardScreen component', () => {
-  it('should display the text only from statuses', () => {
+  it('should display the text only from statuses and does not display posts', () => {
     const modules = {
       twentyFourSeven: {
         namespaced: true,
@@ -51,49 +58,73 @@ describe('TFSDashboardScreen component', () => {
 
     expect(wrapper.text()).toMatch('Picking')
     expect(wrapper.text()).toMatch('Summary')
+    expect(wrapper.findAllComponents(TFSPostCard).length).toEqual(0)
   })
 
   describe('when the items be reveived', () => {
-    it('should display the cards with content', () => {
-      const modules = {
-        twentyFourSeven: {
-          namespaced: true,
-          actions: {[action.GET_TFS_ITEMS]: jest.fn()},
-          state: {
-            items: {
-              Summary: {
-                count: 1,
-                next: null,
-                previous: null,
-                results: [
-                  {
-                    header: 'Test post',
-                    id: 1,
-                    original_content: 'qwerty',
-                    post: {
-                      feedlink__alexaglobalrank: '12345',
-                      entry_summary: 'Summary',
-                      entry_title: 'Test post',
-                      id: 1,
-                      sentiment: 'neutral',
-                    },
-                    project: 7,
-                    status: 'Summary',
-                    text: 'test123',
-                  },
-                ],
-              },
-            },
-            sortType: {label: 'Latest'},
-          },
-        },
-      }
-      const store = createNewStore({}, {modules})
-      const wrapper = createWrapper(store)
+    const item = {
+      header: 'Test post',
+      id: 1,
+      original_content: 'qwerty',
+      linked_items: [1, 2],
+      post: {
+        feedlink__alexaglobalrank: '12345',
+        entry_summary: 'Summary',
+        entry_title: 'Test post',
+        id: 1,
+        sentiment: 'neutral',
+      },
+      project: 7,
+      status: 'Summary',
+      text: 'test123',
+    }
 
+    const modules = {
+      twentyFourSeven: {
+        namespaced: true,
+        actions: {
+          [action.GET_TFS_ITEMS]: jest.fn(),
+          [action.CLEAR_TFS_RELATED_CONTENT]: jest.fn(),
+          [action.GET_TFS_RELATED_CONTENT]: jest.fn(),
+        },
+        state: {
+          relatedContent: [item],
+          items: {
+            Summary: {
+              count: 1,
+              next: null,
+              previous: null,
+              results: [item],
+            },
+          },
+          sortType: {label: 'Latest'},
+        },
+      },
+    }
+    const store = createNewStore({}, {modules})
+    const wrapper = createWrapper(store)
+
+    it('should display the posts with content', () => {
       expect(wrapper.text()).toMatch('Neutral')
       expect(wrapper.text()).toMatch('Summary')
       expect(wrapper.text()).toMatch('Test post')
+      expect(wrapper.findAllComponents(TFSPostCard).length).toEqual(1)
+    })
+
+    describe('and when the user cliks on the "take to work" button', () => {
+      it('should display the modal window', async () => {
+        expect(wrapper.findAllComponents(TFSWorkingModal).length).toEqual(0)
+
+        await wrapper.setData({
+          postInfo: item,
+        })
+
+        const button = wrapper.find('.work-button')
+        await button.trigger('click')
+
+        expect(wrapper.text()).toMatch('Original content')
+        expect(wrapper.findAllComponents(TFSWorkingModal).length).toEqual(1)
+      })
     })
   })
 })
