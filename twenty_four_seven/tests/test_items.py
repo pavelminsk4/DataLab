bb85cbb1aa7e3e530fa1a58fa6fc144ff3125908
Vadim Kondratiev <vvk@anadeainc.com>
2023-12-21 11:43:09 +0300

    Add text and header in Arabic to TFS items

--- twenty_four_seven/tests/test_items.py
+++ twenty_four_seven/tests/test_items.py
@@ -4,23 +4,57 @@ from django.urls import reverse
 import json
 from common.factories.twenty_four_seven.tfs_project import ProjectTwentyFourSevenFactory
 from common.factories.twenty_four_seven.tfs_item import ItemFactory
+from common.factories.post import PostFactory
+
+
+def exclude(data, keys):
+    return {k: v for k, v in data.items() if k not in keys}
 
 
 class ItemTests(APITestCase):
-    def test_item_endpoint(self):
-        project = ProjectTwentyFourSevenFactory()
-        item = ItemFactory(project=project)
-        url = reverse('twenty_four_seven:project-items-detail',
-                      args=(project.pk, item.pk))
-        response = self.client.get(url)
-        self.assertEqual(json.loads(response.content)['is_back'], False)
-        self.assertEqual(json.loads(response.content)['status'], 'PCK')
+    maxDiff = None
+
+    def setUp(self):
+        self.project = ProjectTwentyFourSevenFactory()
+        self.post    = PostFactory()
+        self.item    = ItemFactory(project=self.project, post=self.post)
+        self.path    = f'/api/twenty_four_seven/projects/{self.project.id}/items/{self.item.id}/'
+
+    def test_get_item_details(self):
+        """GET 24/7 project item details should return the serialized item"""
+        response = self.client.get(self.path)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        data = {
-            'is_back': True,
-            'status': 'Irrelevant',
-        }
-        response = self.client.patch(url, data)
+        self.assertEqual(exclude(json.loads(response.content), ['id', 'post']), {
+            'header': 'Header Text',
+            'header_ar': '',
+            'in_work': False,
+            'is_back': False,
+            'linked_items': [],
+            'original_content': None,
+            'project': self.project.id,
+            'social_post': None,
+            'status': 'PCK',
+            'text': 'Description Text',
+            'text_ar': ''
+        })
+
+    def test_patch_item_details(self):
+        """PATCH 24/7 project item details should update the item"""
+        response = self.client.patch(self.path, {
+            'header': 'New Header', 'text': 'New Text', 'header_ar': 'رأس جديد', 'text_ar': 'نص جديد'
+        })
+
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(json.loads(response.content)['is_back'], True)
-        self.assertEqual(json.loads(response.content)['status'], 'Irrelevant')
+        self.assertEqual(exclude(json.loads(response.content), ['id', 'post']), {
+            'header': 'New Header',
+            'header_ar': 'رأس جديد',
+            'in_work': False,
+            'is_back': False,
+            'linked_items': [],
+            'original_content': None,
+            'project': self.project.id,
+            'social_post': None,
+            'status': 'PCK',
+            'text': 'New Text',
+            'text_ar': 'نص جديد'
+        })
